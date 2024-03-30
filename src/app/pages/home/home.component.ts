d57f79edcf52b53a68a3d0e2c7a75d6800f9a7ab
Ivan Shutov <vanasutov793@gmail.com>
2024-03-30 10:09:22 +0400

    feat: add article-card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,14 @@ import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { TuiCarouselModule } from '@taiga-ui/kit';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 import { ButtonModule } from '../../components/button/button.module';
 import { ISliderData } from '../../../assets/slider/types/slider-data.interface';
 import { ServiceCardComponent } from '../../components/service-card/service-card.component';
 import { IServiceCard } from '../../../assets/service-card/types/service-card.interface';
 import { ContactUsCardComponent } from '../../components/contact-us-card/contact-us-card.component';
+import { ArticleCardComponent } from '../../components/article-card/article-card.component';
+import { IArticleCardData } from '../../../assets/article-card/types/article-card-data.interface';
 
 @Component({
     selector: 'app-home',
@@ -17,6 +20,7 @@ import { ContactUsCardComponent } from '../../components/contact-us-card/contact
         CommonModule,
         ServiceCardComponent,
         ContactUsCardComponent,
+        ArticleCardComponent,
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
@@ -28,7 +32,10 @@ export class HomeComponent {
     readonly slidersData$ = this.http.get<ISliderData[]>(
         'assets/slider/slider-data.json',
     );
-    readonly cardsData$ = this.http.get<IServiceCard[]>(
+    readonly serviceCardsData$ = this.http.get<IServiceCard[]>(
         'assets/service-card/service-card-data.json',
     );
+    readonly articlesCardsData$ = this.http
+        .get<IArticleCardData[]>('assets/article-card/article-card-data.json')
+        .pipe(map((articlesCardsData) => articlesCardsData.slice(0, 6)));
 }

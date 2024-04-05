5970921d177a968ec27a5306a0b5f2635bd92082
Ivan Shutov <vanasutov793@gmail.com>
2024-04-05 17:22:46 +0400

    fix: connection TUI_SANITIZER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { TUI_SANITIZER } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
     title = 'wood-products-shop';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { TuiRootModule } from '@taiga-ui/core';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { TUI_SANITIZER, TuiRootModule } from '@taiga-ui/core';
+import {
+    ApplicationConfig,
+    importProvidersFrom,
+    isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,7 +14,18 @@ import {
     provideHttpClient,
     withFetch,
 } from '@angular/common/http';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TranslateModule } from '@ngx-translate/core';
+import { storeReducer } from './store/reducer';
 import { routes } from './app.routes';
+
+const devtoolsInstruments = [];
+
+if (isDevMode()) {
+    devtoolsInstruments.push(StoreDevtoolsModule.instrument());
+}
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,8 +33,13 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideClientHydration(),
         provideAnimationsAsync(),
-        importProvidersFrom(TuiRootModule),
         importProvidersFrom(HttpClientModule),
         provideHttpClient(withFetch()),
+        { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+        importProvidersFrom(TuiRootModule),
+        importProvidersFrom(StoreModule.forRoot(storeReducer)),
+        importProvidersFrom(StoreDevtoolsModule.instrument()),
+        importProvidersFrom(...devtoolsInstruments),
+        importProvidersFrom(TranslateModule.forRoot({})),
     ],
 };

98430cde9c3f49c9073b512c0c471bf26f315161
Ivan Shutov <vanasutov793@gmail.com>
2024-04-11 17:46:19 +0400

    fix: categories list

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { map } from 'rxjs';
 import { LogoComponent } from '../logo/logo.component';
 import { PHONE_NUMBER } from '../../shared/contact-info/phone-number.token';
 import { SUPPORT_EMAIL } from '../../shared/contact-info/support-email.token';
 import { ADDRESS } from '../../shared/contact-info/address.token';
 import { METRO_STATION } from '../../shared/contact-info/metro-station.token';
 import { ButtonModule } from '../button/button.module';
+import { IState } from '../../store/reducer';
+import { productsFeatureSelector } from '../../store/products/products.selector';
 
 @Component({
     selector: 'app-footer',
@@ -19,7 +23,13 @@ import { ButtonModule } from '../button/button.module';
 export class FooterComponent {
     readonly currentYear = new Date().getFullYear();
 
+    readonly categories$ = this.store$.pipe(
+        select(productsFeatureSelector),
+        map(({ categories }) => categories),
+    );
+
     constructor(
+        private readonly store$: Store<IState>,
         @Inject(SUPPORT_EMAIL) readonly suppurtEmail: string,
         @Inject(PHONE_NUMBER) readonly phoneNumber: string,
         @Inject(ADDRESS) readonly address: string,

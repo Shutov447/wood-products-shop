dc73fba52fbc0b63b836bd2330df5611f5515567
Ivan Shutov <vanasutov793@gmail.com>
2024-04-14 04:20:01 +0400

    refactor: icon-button component

--- src/app/components/button/components/icon-button/icon-button.component.ts
+++ src/app/components/button/components/icon-button/icon-button.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { tuiHexToRGBA } from '@taiga-ui/cdk';
 import { IconButton } from '../../shared/types/icon-button.type';
 import { ElemHoverService } from '../../shared/services/elem-hover/elem-hover.service';
 
@@ -12,9 +13,14 @@ import { ElemHoverService } from '../../shared/services/elem-hover/elem-hover.se
 export class IconButtonComponent {
     @Input({ required: true }) icon: IconButton | null = null;
 
-    readonly white = '#ffffff';
-    readonly frangipani = '#ffd6a8';
+    @Input() defaultColor = '#ffffff';
+    @Input() hoverColor = '#ffd6a8';
+    @Input() bgColor = '#3b3937';
     readonly isHovered$ = this.elemHoverService.isHovered$;
 
     constructor(private readonly elemHoverService: ElemHoverService) {}
+
+    hexToRGBA(hex: string, alpha?: number): string {
+        return tuiHexToRGBA(hex, alpha);
+    }
 }

e60953d8de2159d9afc37fc9444c6482fa955597
Ivan Shutov <vanasutov793@gmail.com>
2024-04-22 23:13:58 +0400

    refactor(catalog component): delete unnecessary code

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { UrlSegmentsVisualizerComponent } from '../../components/url-segments-visualizer/url-segments-visualizer.component';
 import { productsFeatureSelector } from '../../store/products/products.selector';
 import { IState } from '../../store/reducer';
-import { IProduct } from '../../../assets/products/types/product.interface';
 import { ProductsService } from '../../shared/products/products.service';
 import { ICategotyIntroImg } from '../../../assets/category-intro-img/types/category-intro-img.interface';
 import { ButtonModule } from '../../components/button/button.module';
@@ -41,11 +40,4 @@ export class CatalogComponent {
         private readonly productsService: ProductsService,
         private readonly http: HttpClient,
     ) {}
-
-    getIntroImg(category: IProduct['category']): string {
-        const introImg = this.productsService.getProducts(category, 1)[0]
-            ?.photos[0];
-
-        return introImg;
-    }
 }

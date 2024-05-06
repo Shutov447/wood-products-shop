388b2bdf5d380abc3b2b59476297535b8fab65b4
Ivan Shutov <vanasutov793@gmail.com>
2024-05-07 03:27:32 +0400

    feat: add characteristic  component

---
+++ src/app/components/characteristics/characteristics.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ICharacteristic } from '../../../assets/products/types/product.interface';
+
+@Component({
+    selector: 'app-characteristics',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './characteristics.component.html',
+    styleUrl: './characteristics.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CharacteristicsComponent {
+    @Input() title: string | null = null;
+    @Input({ required: true }) characteristics: ICharacteristic[] | undefined;
+}

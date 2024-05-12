422a95ac0a41b281f8ba45a0138229435e08198c
Ivan Shutov <vanasutov793@gmail.com>
2024-05-13 03:39:37 +0400

    feat: add dialog component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ProductsService } from '../../shared/products/products.service';
 import { UrlSegmentsVisualizerComponent } from '../../components/url-segments-visualizer/url-segments-visualizer.component';
 import { ImageGalleryComponent } from '../../components/image-gallery/image-gallery.component';
@@ -8,6 +9,8 @@ import { ButtonModule } from '../../components/button/button.module';
 import { CounterComponent } from '../../components/counter/counter.component';
 import { ContactUsCardComponent } from '../../components/contact-us-card/contact-us-card-default/contact-us-card.component';
 import { CharacteristicsComponent } from '../../components/characteristics/characteristics.component';
+import { DialogComponent } from '../../components/dialog/dialog.component';
+import { AlertTriangleIconComponent } from '../../components/alert-triangle-icon/alert-triangle-icon.component';
 
 @Component({
     selector: 'app-product',
@@ -20,18 +23,27 @@ import { CharacteristicsComponent } from '../../components/characteristics/chara
         CounterComponent,
         ContactUsCardComponent,
         CharacteristicsComponent,
+        DialogComponent,
+        ReactiveFormsModule,
+        AlertTriangleIconComponent,
+        RouterModule,
     ],
     templateUrl: './product.component.html',
     styleUrl: './product.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductComponent {
+export class ProductComponent extends ContactUsCardComponent {
     product$ = this.productsService.product$;
+
+    productsCount = 1;
+    isOrderFormPopup = true;
 
     constructor(
         private readonly activatedRoute: ActivatedRoute,
         private readonly productsService: ProductsService,
+        private readonly fb: FormBuilder,
     ) {
+        super(fb);
         this.activatedRoute.paramMap.subscribe((paramMap) => {
             const productName = paramMap.get('product');
 
@@ -39,8 +51,15 @@ export class ProductComponent {
         });
     }
 
-    getProductNumber(productNumber: number) {
-        // eslint-disable-next-line no-console
-        console.log(productNumber);
+    setProductNumber(productNumber: number) {
+        this.productsCount = productNumber;
+    }
+
+    getPopupHiddenState(hiddenState: boolean) {
+        if (!this.isOrderFormPopup && hiddenState) {
+            this.isOrderFormPopup = false;
+        } else {
+            this.isOrderFormPopup = true;
+        }
     }
 }

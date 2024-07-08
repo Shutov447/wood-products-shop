988839cbf60206db9ed7e787f733f2e343865f25
Ivan Shutov <vanasutov793@gmail.com>
2024-07-08 18:25:31 +0400

    test: elem-hover directive and service

--- src/shared/components/button/lib/elem-hover-directive/elem-hover.directive.spec.ts
+++ src/shared/components/button/lib/elem-hover-directive/elem-hover.directive.spec.ts
@@ -1,9 +1,92 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { PushPipe } from '@ngrx/component';
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ElemHoverDirective } from './elem-hover.directive';
+import { ElemHoverService } from '../../model';
 
+@Component({
+    standalone: true,
+    imports: [ElemHoverDirective, PushPipe],
+    providers: [ElemHoverService],
+    template: `
+        <div
+            appElemHover
+            id="testElem"
+        ></div>
+        <div id="hoverElem">
+            {{ (isHovered$ | ngrxPush) ? 'testHover' : 'testHoverOut' }}
+        </div>
+    `,
+})
+class ForElemHoverDirectiveTestComponent {
+    readonly isHovered$ = this.elemHoverService.isHovered$;
+
+    constructor(private readonly elemHoverService: ElemHoverService) {}
+}
 describe('ElemHoverDirective', () => {
-    it('should create an instance', () => {
-        const directive = new ElemHoverDirective();
+    let fixture: ComponentFixture<ForElemHoverDirectiveTestComponent>;
+    let directive: ElemHoverDirective;
 
+    beforeEach(() => {
+        fixture = TestBed.configureTestingModule({
+            imports: [ForElemHoverDirectiveTestComponent],
+            providers: [ElemHoverService],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).createComponent(ForElemHoverDirectiveTestComponent);
+        directive = new ElemHoverDirective(new ElemHoverService());
+        fixture.detectChanges();
+    });
+
+    it('should create an instance', () => {
         expect(directive).toBeTruthy();
+    });
+
+    describe('DOM Manipulation with ForElemHoverDirectiveTestComponent', () => {
+        let testElem: HTMLElement;
+        let hoverElem: HTMLElement;
+
+        beforeEach(() => {
+            testElem = fixture.debugElement.query(
+                By.css('#testElem'),
+            ).nativeElement;
+            hoverElem = fixture.debugElement.query(
+                By.css('#hoverElem'),
+            ).nativeElement;
+        });
+
+        it('при mouseover event на testElem, hoverElem должен содержать "testHover"', () => {
+            testElem.dispatchEvent(new Event('mouseover'));
+            fixture.detectChanges();
+
+            expect(hoverElem.textContent?.trim()).toBe('testHover');
+        });
+
+        it('при mouseout event на testElem, hoverElem должен содержать "testHoverOut"', () => {
+            testElem.dispatchEvent(new Event('mouseout'));
+            fixture.detectChanges();
+
+            expect(hoverElem.textContent?.trim()).toBe('testHoverOut');
+        });
+
+        it('при mouseover event должен вызываться onMouseover method', () => {
+            jest.spyOn(ElemHoverDirective.prototype as any, 'onMouseover');
+            testElem.dispatchEvent(new Event('mouseover'));
+            fixture.detectChanges();
+
+            const onMouseover = (ElemHoverDirective.prototype as any)
+                .onMouseover;
+
+            expect(onMouseover).toHaveBeenCalledTimes(1);
+        });
+
+        it('при mouseout event должен вызываться onMouseout method', () => {
+            jest.spyOn(ElemHoverDirective.prototype as any, 'onMouseout');
+            testElem.dispatchEvent(new Event('mouseout'));
+            const onMouseout = (ElemHoverDirective.prototype as any).onMouseout;
+
+            expect(onMouseout).toHaveBeenCalledTimes(1);
+        });
     });
 });
--- src/shared/components/button/model/elem-hover-service/elem-hover.service.spec.ts
+++ src/shared/components/button/model/elem-hover-service/elem-hover.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ElemHoverService } from './elem-hover.service';
 
 describe('ElemHoverService', () => {
     let service: ElemHoverService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({ providers: [ElemHoverService] });
+
         service = TestBed.inject(ElemHoverService);
     });
 
-    it('should be created', () => {
+    test('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('setHover$', () => {
+        let isHovered: boolean | undefined;
+
+        beforeEach(() => {
+            isHovered = undefined;
+        });
+
+        test('передает в поток false', () => {
+            service.setHover$(false);
+            service.isHovered$.subscribe((res) => {
+                isHovered = res;
+            });
+
+            expect(isHovered).toBe(false);
+        });
+
+        test('передает в поток true', () => {
+            service.setHover$(true);
+            service.isHovered$.subscribe((res) => {
+                isHovered = res;
+            });
+
+            expect(isHovered).toBe(true);
+        });
     });
 });
